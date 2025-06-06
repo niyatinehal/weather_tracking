0b37f298ea2c0386b7054fafbe15520e73a0e9ff
NiyatiTDG <58203867+niyatinehal@users.noreply.github.com>
2025-06-06 05:35:37 +0530

    deployment changes

--- src/api/forecastAPI.js
+++ src/api/forecastAPI.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // const API_KEY = process.env.VITE_WEATHER_API_KEY;
 const API_KEY = "e9fdad8bd33b4ddea02115902250506";
-const BASE_URL = 'http://api.weatherapi.com/v1';
+const BASE_URL = 'https://api.weatherapi.com/v1';
 export const getWeatherforecast = async (city) => {
   const res = await axios.get(`${BASE_URL}/forecast.json?key=${API_KEY}&q=${city}&days=7&aqi=no&alerts=no&units=metric`);
   return res.data;
--- src/api/weatherAPI.js
+++ src/api/weatherAPI.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // const API_KEY = process.env.VITE_WEATHER_API_KEY;
 const API_KEY = "e9fdad8bd33b4ddea02115902250506";
-const BASE_URL = 'http://api.weatherapi.com/v1';
+const BASE_URL = 'https://api.weatherapi.com/v1';
 export const getWeatherByCity = async (city) => {
   const res = await axios.get(`${BASE_URL}/current.json?key=${API_KEY}&q=${city}&units=metric`);
   return {
